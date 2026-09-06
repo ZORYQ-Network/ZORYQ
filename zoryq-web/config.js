d7d3b24c07bcee1ffb0a615f37178335ab23c02d
michaeljohnnysilva9-lgtm <michaeljohnnysilva9@gmail.com>
2026-09-06 11:32:48 -0300

    feat: activate live ZORYQ Testnet contracts in web config

--- zoryq-web/config.js
+++ zoryq-web/config.js
@@ -6,11 +6,12 @@ window.ZORYQ_CONFIG={
   rpcBase:'https://zoryq-evm-node-live-production.up.railway.app',
   rpcUrl:'https://zoryq-evm-node-live-production.up.railway.app/rpc',
   explorerUrl:'https://zoryq-testnet.vercel.app/explorer.html',
-  rewardRegistry:'0x0000000000000000000000000000000000000000',
-  questRegistry:'0x0000000000000000000000000000000000000000',
-  questCompletionRegistry:'0x0000000000000000000000000000000000000000',
-  stakeContract:'0x0000000000000000000000000000000000000000',
-  testToken:'0x0000000000000000000000000000000000000000',
-  swapContract:'0x0000000000000000000000000000000000000000',
+  rewardRegistry:'0xE8c01A67735B47Ba3318e96266F0b8bef39710ad',
+  questRegistry:'0x00e8BDa4280e340f75beEEAAcd68C4A6E6828a5a',
+  questCompletionRegistry:'0x2f2378ffdFA02622baF05523901de9a9b7f4f2e3',
+  stakeContract:'0xa1294BeC35184Ac9Ad2f16F3DCfd4de276833499',
+  testToken:'0xdDeE4ECD600aC501B2A6b2A4e98aC180EbF79f94',
+  swapContract:'0xa16AaA3F4df98D312dD7D146E4d0116967F0F7bf',
+  adminTreasury:'0xc0e03982fb8615ddf8b8fabd27e5a35541e12f33',
   scoreEpoch:1
 };
