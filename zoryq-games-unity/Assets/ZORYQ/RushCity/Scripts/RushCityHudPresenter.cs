861df6163ab73db70c30a3124c02a030d40364ff
michaeljohnnysilva9-lgtm <michaeljohnnysilva9@gmail.com>
2026-09-12 14:03:56 -0300

    feat(rush-city): present world events in HUD

--- zoryq-games-unity/Assets/ZORYQ/RushCity/Scripts/RushCityHudPresenter.cs
+++ zoryq-games-unity/Assets/ZORYQ/RushCity/Scripts/RushCityHudPresenter.cs
@@ -5,8 +5,8 @@ namespace Zoryq.Play.RushCity
 {
     public sealed class RushCityHudPresenter : MonoBehaviour
     {
-        Text _score,_zq,_combo,_district,_power,_toast;
-        Image _chaseFill,_flowFill;
+        Text _score,_zq,_combo,_district,_power,_event,_toast;
+        Image _chaseFill,_flowFill,_eventFill;
         float _toastUntil;
         Font _font;
 
@@ -18,6 +18,8 @@ namespace Zoryq.Play.RushCity
             RushCityGameEvents.PowerUpActivated+=OnPowerUp;
             RushCityGameEvents.RouteChosen+=OnRoute;
             RushCityGameEvents.MissionCompleted+=OnMission;
+            RushCityGameEvents.WorldEventChanged+=OnWorldEvent;
+            RushCityGameEvents.WorldEventRewarded+=OnWorldEventReward;
         }
 
         void OnDisable()
@@ -26,6 +28,8 @@ namespace Zoryq.Play.RushCity
             RushCityGameEvents.PowerUpActivated-=OnPowerUp;
             RushCityGameEvents.RouteChosen-=OnRoute;
             RushCityGameEvents.MissionCompleted-=OnMission;
+            RushCityGameEvents.WorldEventChanged-=OnWorldEvent;
+            RushCityGameEvents.WorldEventRewarded-=OnWorldEventReward;
         }
 
         void Build()
@@ -45,11 +49,14 @@ namespace Zoryq.Play.RushCity
             _combo=TextNode(canvasGo.transform,"Combo","",30,TextAnchor.MiddleCenter,new Vector2(.32f,.865f),new Vector2(.68f,.91f));
             _district=TextNode(canvasGo.transform,"District","NEO DOWNTOWN",24,TextAnchor.MiddleLeft,new Vector2(.04f,.90f),new Vector2(.55f,.935f));
             _power=TextNode(canvasGo.transform,"Power","",24,TextAnchor.MiddleRight,new Vector2(.48f,.90f),new Vector2(.96f,.935f));
-            _toast=TextNode(canvasGo.transform,"Toast","",34,TextAnchor.MiddleCenter,new Vector2(.16f,.72f),new Vector2(.84f,.79f));
+            _event=TextNode(canvasGo.transform,"WorldEvent","",30,TextAnchor.MiddleCenter,new Vector2(.12f,.755f),new Vector2(.88f,.805f));
+            _event.fontStyle=FontStyle.Bold;
+            _toast=TextNode(canvasGo.transform,"Toast","",34,TextAnchor.MiddleCenter,new Vector2(.16f,.69f),new Vector2(.84f,.75f));
             _toast.fontStyle=FontStyle.Bold;
 
             CreateBar(canvasGo.transform,"ChaseBar",new Vector2(.04f,.835f),new Vector2(.96f,.852f),new Color(.11f,.12f,.17f,.8f),new Color(1f,.08f,.25f,.95f),out _chaseFill);
             CreateBar(canvasGo.transform,"FlowBar",new Vector2(.04f,.81f),new Vector2(.96f,.824f),new Color(.11f,.12f,.17f,.65f),new Color(.05f,.88f,1f,.92f),out _flowFill);
+            CreateBar(canvasGo.transform,"EventBar",new Vector2(.16f,.747f),new Vector2(.84f,.754f),new Color(.11f,.12f,.17f,.45f),new Color(.75f,.18f,1f,.96f),out _eventFill);
         }
 
         void Update()
@@ -62,6 +69,22 @@ namespace Zoryq.Play.RushCity
             _chaseFill.fillAmount=gm.ChasePressure;
             _flowFill.fillAmount=gm.SkillFlow;
             _power.text=PowerText(gm);
+
+            var director=RushCityWorldEventDirector.Instance;
+            if(director&&director.ActiveEvent!=RushWorldEvent.None)
+            {
+                _event.text=$"{EventName(director.ActiveEvent)}   {director.RemainingSeconds:0.0}s";
+                _eventFill.fillAmount=1f-director.Progress;
+                _event.enabled=true;
+                _eventFill.transform.parent.gameObject.SetActive(true);
+            }
+            else
+            {
+                _event.text=string.Empty;
+                _eventFill.fillAmount=0f;
+                _eventFill.transform.parent.gameObject.SetActive(false);
+            }
+
             if(Time.unscaledTime>_toastUntil)_toast.text=string.Empty;
             else
             {
@@ -83,11 +106,24 @@ namespace Zoryq.Play.RushCity
             switch(d){case RushDistrict.CyberHarbor:return "CYBER HARBOR";case RushDistrict.Skyline:return "SKYLINE";case RushDistrict.OldMetro:return "OLD METRO";default:return "NEO DOWNTOWN";}
         }
 
+        string EventName(RushWorldEvent worldEvent)
+        {
+            switch(worldEvent)
+            {
+                case RushWorldEvent.DroneGauntlet:return "DRONE GAUNTLET";
+                case RushWorldEvent.HyperTrain:return "HYPER TRAIN";
+                case RushWorldEvent.StormRush:return "STORM RUSH";
+                default:return string.Empty;
+            }
+        }
+
         void OnNearMiss()=>Toast("NEAR MISS  +FLOW");
         void OnPowerUp(RushPowerUpType type,float duration)=>Toast(type.ToString().ToUpperInvariant()+"  ONLINE");
         void OnRoute(int direction,string district)=>Toast((direction<0?"LEFT ROUTE · ":"RIGHT ROUTE · ")+district.ToUpperInvariant());
         void OnMission(int score,int zq,string mission)=>Toast($"MISSION COMPLETE  +{zq} ZQ");
-        void Toast(string value){_toast.text=value;var c=_toast.color;c.a=1f;_toast.color=c;_toastUntil=Time.unscaledTime+1.55f;}
+        void OnWorldEvent(RushWorldEvent worldEvent,bool active)=>Toast(active?$"{EventName(worldEvent)}  INCOMING":$"{EventName(worldEvent)}  CLEARED");
+        void OnWorldEventReward(RushWorldEvent worldEvent,int score,int zq)=>Toast($"EVENT COMPLETE  +{zq} ZQ  +{score:N0}");
+        void Toast(string value){_toast.text=value;var c=_toast.color;c.a=1f;_toast.color=c;_toastUntil=Time.unscaledTime+1.75f;}
 
         Text TextNode(Transform parent,string name,string value,int size,TextAnchor align,Vector2 min,Vector2 max)
         {
